a881990dcc43d903ec27c78eaae13733f0d44cc5
YisusFin <davidgh@finerioconnect.com>
2023-06-27 17:31:11 -0600

    First commit

---
+++ YisusFramework/1.0.0/YisusFramework.xcframework/ios-arm64_x86_64-simulator/YisusFramework.framework/Headers/YisusFramework.h
@@ -0,0 +1,18 @@
+//
+//  YisusFramework.h
+//  YisusFramework
+//
+//  Created by Jesus G on 24/06/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for YisusFramework.
+FOUNDATION_EXPORT double YisusFrameworkVersionNumber;
+
+//! Project version string for YisusFramework.
+FOUNDATION_EXPORT const unsigned char YisusFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YisusFramework/PublicHeader.h>
+
+
